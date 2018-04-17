d34679b30491745d2705849923a802588f8805d2
osap128 <olivier.sap@hotmail.fr>
2018-04-17 14:24:12 +0200

    gestion conflit mainwindow

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -1,15 +1,32 @@
+/*
+================================================
+* File:         mainwindow.cpp
+* Project:      ClipEdit
+* Creation:     17/04/2018
+* Brief:        Application's main window
+================================================
+*/
+
+// Includes
+// --------
+
+#include <QDebug>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+// Constructor, destructor
+// -----------------------
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ///connection between signals and slots when users select save, open or exports in the menu
-    connect(ui->actionSave,SIGNAL(triggered(bool)),this,SLOT(slot_save(bool)));//save
-    connect(ui->actionOpen,SIGNAL(triggered(bool)),this,SLOT(slot_open(bool)));//open
-    connect(ui->actionExports,SIGNAL(triggered(bool)),this,SLOT(slot_exports(bool)));//exports
+
+    buildMenu();
+    buildToolBar();
+    buildView();
 }
 
 MainWindow::~MainWindow()
@@ -17,20 +34,65 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-///slot save: This function is told when users tells to save data
-void MainWindow::slot_save(bool isok)
+// UI building
+// -----------
+
+///
+/// \brief MainWindow::buildMenu
+/// Initializations and methods calls related to the menu
+///
+void MainWindow::buildMenu()
 {
-    //qDebug()<<"save";
+    connect(ui->actionSave, SIGNAL(triggered(bool)), this, SLOT(save(bool)));
+    connect(ui->actionOpen, SIGNAL(triggered(bool)), this, SLOT(openFile(bool)));
+    connect(ui->actionExports, SIGNAL(triggered(bool)), this, SLOT(exportView(bool)));
 }
 
-///slot open: This function is told when users tells to open data
-void MainWindow::slot_open(bool)
+///
+/// \brief MainWindow::buildToolBar
+/// Initializations and methods calls related to the toolbar
+///
+void MainWindow::buildToolBar()
 {
-    //qDebug()<<"open";
+
 }
 
-///slot exports: This function is told when users tells to export data
-void MainWindow::slot_exports(bool)
+///
+/// \brief MainWindow::buildView
+/// Initializations and methods calls related to the main view
+///
+void MainWindow::buildView()
 {
-    //qDebug()<<"exports";
+    int offset = 600;
+    m_scene.setSceneRect((-width()-offset)/2, (-height()-offset)/2, width()+offset, height()+offset);
+
+    ui->graphicsView->setScene(&m_scene);
+}
+
+// Slots
+// -----
+
+///
+/// \brief save
+/// This slot is called on save
+///
+void MainWindow::save(bool)
+{
+    // To do
+}
+
+///
+/// \brief openFile
+/// This slot is called on file open
+void MainWindow::openFile(bool)
+{
+    // To do
+}
+
+///
+/// \brief exportView
+/// This slot is called on export
+void MainWindow::exportView(bool)
+{
+    // To do
 }
