b5842c212d05992c3217eacea0847a024a1d5804
osap128 <olivier.sap@hotmail.fr>
2018-04-17 11:20:37 +0200

    ajout connect dans cpp pour save open exports

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -6,6 +6,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ///connection between signals and slots when users select save, open or exports in the menu
+    connect(ui->actionSave,SIGNAL(triggered(bool)),this,SLOT(slot_save(bool)));//save
+    connect(ui->actionOpen,SIGNAL(triggered(bool)),this,SLOT(slot_open(bool)));//open
+    connect(ui->actionExports,SIGNAL(triggered(bool)),this,SLOT(slot_exports(bool)));//exports
 }
 
 MainWindow::~MainWindow()
@@ -16,17 +20,17 @@ MainWindow::~MainWindow()
 ///slot save: This function is told when users tells to save data
 void MainWindow::slot_save(bool isok)
 {
-    qDebug()<<"save";
+    //qDebug()<<"save";
 }
 
 ///slot open: This function is told when users tells to open data
 void MainWindow::slot_open(bool)
 {
-    qDebug()<<"open";
+    //qDebug()<<"open";
 }
 
 ///slot exports: This function is told when users tells to export data
 void MainWindow::slot_exports(bool)
 {
-    qDebug()<<"exports";
+    //qDebug()<<"exports";
 }
