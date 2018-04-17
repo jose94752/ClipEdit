7c32727600fea5575efad503e2bbe025fe81911e
osap128 <olivier.sap@hotmail.fr>
2018-04-17 11:13:23 +0200

    ajout slots save open exports

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -12,3 +12,21 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+///slot save: This function is told when users tells to save data
+void MainWindow::slot_save(bool isok)
+{
+    qDebug()<<"save";
+}
+
+///slot open: This function is told when users tells to open data
+void MainWindow::slot_open(bool)
+{
+    qDebug()<<"open";
+}
+
+///slot exports: This function is told when users tells to export data
+void MainWindow::slot_exports(bool)
+{
+    qDebug()<<"exports";
+}
