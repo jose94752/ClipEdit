51a137e9cfd911d7c563b07127705dcef4eabfe2
osap128 <olivier.sap@hotmail.fr>
2018-04-18 12:30:39 +0200

    couleur

--- sources/Classes/colorbutton.cpp
+++ sources/Classes/colorbutton.cpp
@@ -4,6 +4,7 @@
 #include <QDebug>
 #include <QColorDialog>
 #include <QPaintEvent>
+#include <QPainter>
 
 ColorButton::ColorButton(QWidget* widget):QToolButton(widget)
 {
@@ -25,11 +26,17 @@ void ColorButton::setColor(QColor vcolor)
 
 void ColorButton::slotChoiseColor(bool)
 {
-    qDebug()<<"cliqué";
-    color = QColorDialog::getColor(color, this );
+      color = QColorDialog::getColor(color, this );
 }
 
 void ColorButton::paintEvent( QPaintEvent *event )
 {
-qDebug()<<this->width()<<this->height();
+QToolButton::paintEvent(event) ;
+    QPainter painter(this);
+    QBrush  b(color);
+    QPen p(color);
+    painter.setPen(p);
+    painter.setBrush(b);
+    painter.drawEllipse(this->width()/2,this->height()/2,this->width()/3,this->height()/3);
+
 }
