6a7017ddd0178c6a22eef09cf80060542f56bc1f
cmoliard <foetus333@gmail.com>
2018-04-18 06:09:15 +0200

    update project architecture

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -15,22 +15,60 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+#define INIT_BUTTON(BUTTON_ID, TEXT, PIXMAP) \
+    list_buttons[BUTTON_ID]->set_text(TEXT); \
+    list_buttons[BUTTON_ID]->set_pixmap(PIXMAP); \
+    list_buttons[BUTTON_ID]->button_id = BUTTON_ID; \
+    list_buttons[BUTTON_ID]->unselect(true); \
+     \
+    connect(list_buttons[BUTTON_ID], SIGNAL( on_click(unsigned char) ), this, SLOT( event_onClick(unsigned char) ));
+
+
+#define INIT_SELECTED_BUTTON(BUTTON_ID, TEXT, PIXMAP) \
+    list_buttons[BUTTON_ID]->set_text(TEXT); \
+    list_buttons[BUTTON_ID]->set_pixmap(PIXMAP); \
+    list_buttons[BUTTON_ID]->select(); \
+    list_buttons[BUTTON_ID]->button_id = BUTTON_ID; \
+    selected_button = BUTTON_ID; \
+     \
+    connect(list_buttons[BUTTON_ID], SIGNAL( on_click(unsigned char) ), this, SLOT( event_onClick(unsigned char) ));
+
+
 // Constructor, destructor
 // -----------------------
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+    QMainWindow(parent), ui(new Ui::MainWindow), form_arrows(canvas),
+    form_charts(canvas), form_cliparts(canvas), form_layers(canvas),
+    form_bullets(canvas), form_pictures(canvas), form_screenshots(canvas),
+    form_textboxes(canvas) {
+
     ui->setupUi(this);
+
+    splitter.setObjectName(QStringLiteral("splitter"));
+    splitter.setOrientation(Qt::Horizontal);
+
+    canvas.setObjectName(QStringLiteral("canvas"));
+    canvas.setMinimumSize(QSize(400, 0));
+    canvas.init();
+
+    splitter.addWidget(&canvas);
+
+    stacked_widget.setObjectName(QStringLiteral("stacked_widget"));
+    splitter.addWidget(&stacked_widget);
+
+    ui->centralLayout->addWidget(&splitter);
 
     init();
 }
+
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
 
 // UI building
 // -----------
@@ -39,7 +77,6 @@ void MainWindow::init()
 {
     buildMenu();
     buildToolBar();
-    buildView();
 }
 
 ///
@@ -48,52 +85,122 @@ void MainWindow::init()
 ///
 void MainWindow::buildMenu()
 {
-    connect(ui->actionSave, SIGNAL(triggered(bool)), this, SLOT(save(bool)));
-    connect(ui->actionSave, SIGNAL(triggered(bool)), this, SLOT(saveAs(bool)));
-    connect(ui->actionOpen, SIGNAL(triggered(bool)), this, SLOT(openFile(bool)));
-    connect(ui->actionExports, SIGNAL(triggered(bool)), this, SLOT(exportView(bool)));
+    // Connects
+    connect(ui->actionSave,    SIGNAL( triggered(bool) ), this, SLOT( save(bool) ));
+    connect(ui->actionSave,    SIGNAL( triggered(bool) ), this, SLOT( saveAs(bool) ));
+    connect(ui->actionOpen,    SIGNAL( triggered(bool) ), this, SLOT( openFile(bool) ));
+    connect(ui->actionExports, SIGNAL( triggered(bool) ), this, SLOT( exportView(bool) ));
+
+    connect(ui->actionArrow,           SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
+    connect(ui->actionChart,           SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
+    connect(ui->actionClipart,         SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
+    connect(ui->actionNumberedBullets, SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
+    connect(ui->actionPicture,         SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
+    connect(ui->actionScreenshot,      SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
+    connect(ui->actionTextBox,         SIGNAL( triggered(bool) ), this, SLOT( changeLateralForm(bool) ));
 }
 
 ///
 /// \brief MainWindow::buildToolBar
 /// Initializations and methods calls related to the toolbar
 ///
-void MainWindow::buildToolBar()
-{
-    ui->mainToolBar->addAction(ui->actionArrow);
-    ui->mainToolBar->addAction(ui->actionChart);
-    ui->mainToolBar->addAction(ui->actionClipart);
-    ui->mainToolBar->addAction(ui->actionNumberedBullets);
-    ui->mainToolBar->addAction(ui->actionPicture);
-    ui->mainToolBar->addAction(ui->actionTextBox);
-    ui->mainToolBar->addSeparator();
-    ui->mainToolBar->addAction(ui->actionScreenshot);
-    ui->mainToolBar->addSeparator();
-    ui->mainToolBar->addAction(ui->actionNewLayer);
+void MainWindow::buildToolBar() {
+    list_buttons[BUTTON_ID_ARROW]      = ui->button_arrow;
+    list_buttons[BUTTON_ID_CHART]      = ui->button_chart;
+    list_buttons[BUTTON_ID_BULLET]     = ui->button_bullet;
+    list_buttons[BUTTON_ID_CLIPART]    = ui->button_clipart;
+    list_buttons[BUTTON_ID_PICTURE]    = ui->button_picture;
+    list_buttons[BUTTON_ID_TEXTBOX]    = ui->button_textbox;
+    list_buttons[BUTTON_ID_SCREENSHOT] = ui->button_screenshot;
+    list_buttons[BUTTON_ID_LAYERS]     = ui->button_layers;
 
-    // Connects
-    connect(ui->actionArrow, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
-    connect(ui->actionChart, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
-    connect(ui->actionClipart, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
-    connect(ui->actionNumberedBullets, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
-    connect(ui->actionPicture, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
-    connect(ui->actionScreenshot, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
-    connect(ui->actionTextBox, SIGNAL(triggered(bool)), this, SLOT(changeLateralForm(bool)));
+    INIT_BUTTON(BUTTON_ID_ARROW,      "Draw\nArrow",      ":/icons/icons/arrow-icon.png")
+    INIT_BUTTON(BUTTON_ID_CHART,      "Draw\nChart",      ":/icons/icons/chart-icon.png")
+    INIT_BUTTON(BUTTON_ID_BULLET,     "Draw\nBullet",     ":/icons/icons/numbered-bullet-icon.png")
+    INIT_SELECTED_BUTTON(BUTTON_ID_CLIPART, "Insert\nClipart", ":/icons/icons/clipart-icon.png")
+    INIT_BUTTON(BUTTON_ID_PICTURE,    "Insert\nPicture",  ":/icons/icons/chart-icon.png")
+    INIT_BUTTON(BUTTON_ID_TEXTBOX,    "Insert\nTextbox",  ":/icons/icons/textbox-icon.png")
+    INIT_BUTTON(BUTTON_ID_SCREENSHOT, "Take\nScreenshot", ":/icons/icons/screenshot-icon.png")
+    INIT_BUTTON(BUTTON_ID_LAYERS,     "Manage\nLayers",   ":/icons/icons/layer-icon.png")
+
+//  Remove all useless pages
+    for(int page = 0; page < stacked_widget.count(); ++page) {
+        QWidget* widget = stacked_widget.widget(page);
+        stacked_widget.removeWidget(widget);
+        widget->deleteLater();
+    }
+
+    list_indexes[BUTTON_ID_ARROW]      = stacked_widget.addWidget(&form_arrows);
+    list_indexes[BUTTON_ID_CHART]      = stacked_widget.addWidget(&form_charts);
+    list_indexes[BUTTON_ID_BULLET]     = stacked_widget.addWidget(&form_bullets);
+    list_indexes[BUTTON_ID_CLIPART]    = stacked_widget.addWidget(&form_cliparts);
+    list_indexes[BUTTON_ID_PICTURE]    = stacked_widget.addWidget(&form_pictures);
+    list_indexes[BUTTON_ID_TEXTBOX]    = stacked_widget.addWidget(&form_textboxes);
+    list_indexes[BUTTON_ID_SCREENSHOT] = stacked_widget.addWidget(&form_screenshots);
+    list_indexes[BUTTON_ID_LAYERS]     = stacked_widget.addWidget(&form_layers);
+
+    stacked_widget.setCurrentIndex(list_indexes[selected_button]);
 }
 
-///
-/// \brief MainWindow::buildView
-/// Initializations and methods calls related to the main view
-///
-void MainWindow::buildView()
-{
-    int offset = 600;
-    m_scene.setSceneRect((-width()-offset)/2, (-height()-offset)/2, width()+offset, height()+offset);
-    ui->graphicsView->setScene(&m_scene);
-}
 
 // Slots
 // -----
+
+///
+/// \brief MainWindow::changeLateralForm
+/// Change stacked widget view depending on the action clicked
+void MainWindow::changeLateralForm(bool)
+{
+    if (sender() == ui->actionArrow) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_ARROW]);
+    } else if (sender() == ui->actionChart) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_CHART]);
+    } else if (sender() == ui->actionClipart) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_CLIPART]);
+    } else if (sender() == ui->actionLayers) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_LAYERS]);
+    } else if (sender() == ui->actionNumberedBullets) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_BULLET]);
+    } else if (sender() == ui->actionPicture) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_PICTURE]);
+    } else if (sender() == ui->actionExports) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_PICTURE]);
+    } else if (sender() == ui->actionScreenshot) {
+    } else if (sender() == ui->actionTextBox) {
+        stacked_widget.setCurrentIndex(list_indexes[BUTTON_ID_TEXTBOX]);
+    }
+}
+
+
+void MainWindow::event_onClick(unsigned char clicked_button) {
+    if (selected_button != clicked_button) {
+        list_buttons[clicked_button]->select();
+        list_buttons[selected_button]->unselect();
+
+        selected_button = clicked_button;
+
+        stacked_widget.setCurrentIndex(list_indexes[selected_button]);
+    }
+}
+
+
+///
+/// \brief exportView
+/// This slot is called on export
+void MainWindow::exportView(bool)
+{
+    // To do
+}
+
+
+///
+/// \brief openFile
+/// This slot is called on file open
+void MainWindow::openFile(bool)
+{
+    // To do
+}
+
 
 ///
 /// \brief save
@@ -104,51 +211,11 @@ void MainWindow::save(bool)
     // To do
 }
 
+
 ///
 /// \brief MainWindow::saveAs
 ///
 void MainWindow::saveAs(bool)
 {
     //to do
-}
-
-///
-/// \brief openFile
-/// This slot is called on file open
-void MainWindow::openFile(bool)
-{
-    // To do
-}
-
-///
-/// \brief exportView
-/// This slot is called on export
-void MainWindow::exportView(bool)
-{
-    // To do
-}
-
-///
-/// \brief MainWindow::changeLateralForm
-/// Change stacked widget view depending on the action clicked
-void MainWindow::changeLateralForm(bool)
-{
-    if (sender() == ui->actionArrow)
-        ui->stackedWidget->setCurrentIndex(0);
-    else if (sender() == ui->actionChart)
-        ui->stackedWidget->setCurrentIndex(1);
-    else if (sender() == ui->actionClipart)
-        ui->stackedWidget->setCurrentIndex(2);
-    else if (sender() == ui->actionLayers)
-        ui->stackedWidget->setCurrentIndex(3);
-    else if (sender() == ui->actionNumberedBullets)
-        ui->stackedWidget->setCurrentIndex(4);
-    else if (sender() == ui->actionPicture)
-        ui->stackedWidget->setCurrentIndex(5);
-    else if (sender() == ui->actionExports)
-        ui->stackedWidget->setCurrentIndex(6);
-    else if (sender() == ui->actionScreenshot)
-        ui->stackedWidget->setCurrentIndex(7);
-    else if (sender() == ui->actionTextBox)
-        ui->stackedWidget->setCurrentIndex(8);
 }
--- sources/mainwindow.h
+++ sources/mainwindow.h
@@ -14,47 +14,111 @@
 // --------
 
 #include <QMainWindow>
-#include <QGraphicsScene>
+#include <QtWidgets/QSplitter>
+#include <QtWidgets/QStackedWidget>
 
+#include "Components/One_Menu_Button.h"
+
+#include "Forms/formarrows.h"
+#include "Forms/formcharts.h"
+#include "Forms/formcliparts.h"
+#include "Forms/formlayers.h"
+#include "Forms/formnumberedbullets.h"
+#include "Forms/formpictures.h"
+#include "Forms/formscreenshots.h"
+#include "Forms/formtextboxes.h"
+
+
+// Forward Declaration
 namespace Ui
 {
     class MainWindow;
 }
 
+
 // Class
 // -----
 
-class MainWindow
-    :   public QMainWindow
-{
+class MainWindow: public QMainWindow {
     Q_OBJECT
 
-    public:
 
-        // Constructor, destructor
-        explicit MainWindow(QWidget *parent = 0);
-        ~MainWindow();
+// Constructor & Destructor
+public:
 
-    private:
+    explicit MainWindow(QWidget* /*parent*/ = 0);
 
-        // UI
-        Ui::MainWindow *ui;
-        void init();
-        void buildMenu();
-        void buildToolBar();
-        void buildView();
+    ~MainWindow();
 
-        // Scene
-        QGraphicsScene m_scene;
 
-    private slots:
+// Buttons Ids
+private:
 
-        // Menu and toolbar
-        void save(bool);
-        void saveAs(bool);
-        void openFile(bool);
-        void exportView(bool);
-        void changeLateralForm(bool);
+    enum e_BUTTON_IDS {
+        BUTTON_ID_ARROW = 0,
+        BUTTON_ID_CHART,
+        BUTTON_ID_BULLET,
+        BUTTON_ID_CLIPART,
+        BUTTON_ID_PICTURE,
+        BUTTON_ID_TEXTBOX,
+        BUTTON_ID_SCREENSHOT,
+        BUTTON_ID_LAYERS,
+        NB_BUTTONS
+    };
+
+
+// Main Menu
+private:
+
+  One_Menu_Button* list_buttons[8];
+  int list_indexes[8];
+
+  size_t selected_button;
+
+
+// Interface Menu
+private:
+
+    void init();
+
+    void buildMenu();
+
+    void buildToolBar();
+
+
+private slots:
+
+    void event_onClick(unsigned char);
+
+
+// Data Members
+private:
+
+    // UI
+    Ui::MainWindow* ui;
+
+    QSplitter           splitter;
+    One_Layered_Canvas  canvas;
+    QStackedWidget      stacked_widget;
+
+    FormArrows          form_arrows;
+    FormCharts          form_charts;
+    FormCliparts        form_cliparts;
+    FormLayers          form_layers;
+    FormNumberedBullets form_bullets;
+    FormPictures        form_pictures;
+    FormScreenshots     form_screenshots;
+    FormTextBoxes       form_textboxes;
+
+
+private slots:
+
+    // Menu and toolbar
+    void save(bool);
+    void saveAs(bool);
+    void openFile(bool);
+    void exportView(bool);
+    void changeLateralForm(bool);
 };
 
 #endif
