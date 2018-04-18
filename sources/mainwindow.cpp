bd90b80a0033bc1d885db3074e0d7ac6f4d75ece
formation <nilsen.canot@gmail.com>
2018-04-18 15:28:31 +0200

    Update mainwindow

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -29,7 +29,6 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect (ui->actionNumberedBullets, SIGNAL(triggered(bool)), this, SLOT (slot_NumberedBullets ()));
     init();
 }
 
