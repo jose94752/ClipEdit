fc285ba29da30f86a1cedb6b3c947f2d2e4721dc
Jose Pluquet <jose.pluquet@gmail.com>
2018-04-19 16:54:06 +0200

    Update ArrowsGraphicsItem not functionnal

--- sources/Classes/arrowsgraphicsitem.cpp
+++ sources/Classes/arrowsgraphicsitem.cpp
@@ -13,9 +13,10 @@
 //#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-ArrowsGraphicsItem::ArrowsGraphicsItem()
-
+ArrowsGraphicsItem::ArrowsGraphicsItem(BaseGraphicItem *parent)
+    :   BaseGraphicItem(parent)
 {
+
     // 3 Methods
     // Without anchor point:
     //          we need 2 points on the scene
@@ -34,7 +35,28 @@ QRectF ArrowsGraphicsItem::boundingRect() const
                   20 + penWidth, 20 + penWidth);
 }
 
+QPainterPath ArrowsGraphicsItem::shape() const
+{
+    // To do
+}
+
 void ArrowsGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     painter->drawRoundedRect(-10, -10, 20, 20, 5, 5);
+}
+
+void ArrowsGraphicsItem::updateArrowPosition()
+{
+    // Without anchor point method
+    // To do
+
+    // 1 anchor point method
+    // To do
+
+    //  2 anchors points method
+    /* inactive no constructor for the moment
+    QLineF line(mapFromItem(m_StartItem, 0, 0), mapFromItem(m_EndItem, 0, 0));
+    setLine(line);
+    */
+
 }
