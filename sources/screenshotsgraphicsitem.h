7df7ce212f7b82714685c08f7ebb947d9919dfe7
toumi b17 <btoumi17@hotmail.com>
2018-04-19 13:03:38 +0200

    add second in screenshot.ui

--- sources/screenshotsgraphicsitem.h
+++ sources/screenshotsgraphicsitem.h
@@ -3,13 +3,34 @@
 
 //add
 #include <QGraphicsItem>
+//#include "Items/itemhandler.h"
+#include <QRectF>
+#include <QPointF>
 
 
 
 class ScreenshotsGraphicsItem : public QGraphicsItem
 {
+
 public:
-    ScreenshotsGraphicsItem(QGraphicsItem *parent = 0);
+    // The Constructor and thedestructor
+    explicit ScreenshotsGraphicsItem(QGraphicsItem *parent = 0);
+
+
+    //the virtual destructor makes sure that it gets called even if the class
+    //is getting deleted through a base class pointer.
+    virtual ~ScreenshotsGraphicsItem() {}
+
+public:
+    // Pure virtual methods implementation
+    QRectF boundingRect() const;
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
+    //QRect
+   // QRect::getRect() const;
+
+private:
+    QRectF m_rect_sc;
+
 };
 
 #endif // SCREENSHOTSGRAPHICSITEM_H
