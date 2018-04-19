d2816097892cd13cb04d6e99a1e8a9b3cfaaf1ca
Jose Pluquet <jose.pluquet@gmail.com>
2018-04-19 14:42:10 +0200

    Light update

--- sources/Classes/arrowsgraphicsitem.h
+++ sources/Classes/arrowsgraphicsitem.h
@@ -9,17 +9,30 @@
 class ArrowsGraphicsItem
         : public QGraphicsItem
 {
-    public:
+public:
+    // 4 Type of Thickness outline lines 4 choices possibilities (1 - 4)
+    //enum { Type = UserType + 4 };
+
     ArrowsGraphicsItem();
+
+    // Overriding of the Type
+    //int type() const override { return Type; }
 
     QRectF boundingRect() const;
 
+protected:
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
                QWidget *widget);
 
+
+
 private:
+    //DiagramItem *StI;
+
+
     QColor ItemOutlineColorArrow;
     QColor ItemFillColorArrow;
+
 
 
 private slots:
