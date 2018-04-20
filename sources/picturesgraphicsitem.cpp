41583e094c36dd378fe59b0c17876480cc2d0355
Laurent Mastbaum <laurent_abo@emailaccount.com>
2018-04-20 17:15:29 +0200

    pictures

--- sources/picturesgraphicsitem.cpp
+++ sources/picturesgraphicsitem.cpp
@@ -27,7 +27,6 @@ QRectF PicturesGraphicsItem::boundingRect() const
 
 void PicturesGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-      qDebug() <<"painter  :  "  <<path;
 
   painter->drawPixmap( 0,0, width, height,path);
   //painter->drawText( QRectF (0,0, width, height),"coucou");
