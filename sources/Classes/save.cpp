ccac52a6abea83816be9442e3bed38ef936627e7
osap128 <olivier.sap@hotmail.fr>
2018-04-24 10:19:29 +0200

    getteur sur FormArrow

--- sources/Classes/save.cpp
+++ sources/Classes/save.cpp
@@ -9,9 +9,19 @@
 #include "../Items/textboxitem.h"
 #include "../Items/numberedbulletgraphicitem.h"
 #include "../Items/arrowsgraphicsitem.h"
+#include "../Forms/formarrows.h"
+#include "../Items/graphsgraphicsitem.h"
+/*#define ImageGraphicsItem 65536
+#define TextBoxGraphicsItem 65537
+#define EdgeItem 65539
+#define BulletsItem 65542
+#define PicturesItems 65538*/
 
-QString Save::current_filename="";
+///class to save user projects to a file and to open a project from a file
 
+QString Save::current_filename="";///static variable to store the current store file path and name
+
+///constructor to open a project
 Save::Save(QGraphicsScene* v_scene,QString v_filename)
 {
     current_filename=v_filename;
@@ -20,11 +30,13 @@ Save::Save(QGraphicsScene* v_scene,QString v_filename)
     m_scene=v_scene;
 }
 
+///constructor to save a project when filename is known
 Save::Save(QList<QGraphicsItem *> v_listItems){
     m_listItems=v_listItems;
     m_scene=0;
 }
 
+///constructor to save a file when filename is unknown (save as)
 Save::Save(QList<QGraphicsItem* > v_listItems,QString filename)
 {
     current_filename=filename;
@@ -32,6 +44,7 @@ Save::Save(QList<QGraphicsItem* > v_listItems,QString filename)
     m_scene=0;
 }
 
+///complete file extension .cle if extension is not present in the file name
 QString Save::verifyExtension(QString fileName)
 {
     QStringList strlist=fileName.split(".");
@@ -44,6 +57,7 @@ QString Save::verifyExtension(QString fileName)
     return fileName;
 }
 
+///test if a filename is known by the software
 bool Save::fileNameExists()
 {
     if(current_filename==""){
@@ -53,29 +67,54 @@ bool Save::fileNameExists()
     }
 }
 
-void Save::getPicturesGraphicsItemData(PicturesGraphicsItem *pictureGraphicsItem){
-    //qDebug()<<"picture graphique item";
+///transform TextBoxItem for saving
+void Save::getTextBoxItem(TextBoxItem *textBoxItem,QSettings* settings){
+    qDebug()<<"text boxes";
 }
 
-void Save::getTextBoxItem(TextBoxItem *textBoxItem){
-    //qDebug()<<"text boxes";
-}
-
-void Save::getArrowGraphicsItem(ArrowsGraphicsItem *)
+///transform ArrowsGraphicsItem for saving
+void Save::getArrowGraphicsItem(ArrowsGraphicsItem *arrow,QSettings* settings)
 {
-    //qDebug() arrows graphics items
+    qDebug()<< "arrows graphics items";
+    bool WithoutAnchorPoint;
+    bool OneAnchorPoint;
+    bool TwoAnchorPoints;
+    int ArrowWidth;
+    int ArrowHeight;
+    int r,g,b,a;
+    QColor ArrowOutlineColor;
+    QColor ArrowFillColor;
+    /*arrow->GetInfosArrows(WithoutAnchorPoint,OneAnchorPoint,TwoAnchorPoints,ArrowWidth,ArrowHeight,ArrowOutlineColor,ArrowFillColor);
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/WithoutAnchorPoint"),QString::number(WithoutAnchorPoint));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/OneAnchorPoint"),QString::number(OneAnchorPoint));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/TwoAnchorPoints"),QString::number(TwoAnchorPoints));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowWidth"),QString::number(ArrowWidth));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowHeight"),QString::number(ArrowHeight));
+    ArrowOutlineColor.getRgb(&r,&g,&b,&a);
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/r"),QString::number(r));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/g"),QString::number(g));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/b"),QString::number(b));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/a"),QString::number(a));
+    ArrowFillColor.getRgb(&r,&g,&b,&a);
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowFillColor/r"),QString::number(r));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowFillColor/g"),QString::number(g));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowFillColor/b"),QString::number(b));
+    settings->setValue(QString("item").append(QString::number(countItems)).append("/ArrowFillColor/a"),QString::number(a));*/
 }
 
-void Save::getBulletsGraphicsItems(NumberedBulletGraphicItem *)
+///transform NumberedBulletGraphicItem for saving
+void Save::getBulletsGraphicsItems(NumberedBulletGraphicItem *,QSettings* settings)
 {
-    //code
+    qDebug()<<"NumberedBulletGraphicItem ";
 }
 
-void Save::getPicturesGraphicsItems(PicturesGraphicsItem *)
+///transform PicturesGraphicsItem for saving
+void Save::getPicturesGraphicsItems(PicturesGraphicsItem *,QSettings* settings)
 {
-    //code
+    qDebug()<<"PicturesGraphicsItem";
 }
 
+///set data for differents forms
 void Save::setFormsPoints(FormArrows *f1, FormCharts *f2, FormCliparts *f3, FormLayers *f4, FormNumberedBullets *f5, FormPictures *f6, FormScreenshots *f7, FormTextBoxes *f8)
 {
     m_formArrows=f1;
@@ -88,29 +127,80 @@ void Save::setFormsPoints(FormArrows *f1, FormCharts *f2, FormCliparts *f3, Form
     m_formTextBoxes=f8;
 }
 
-int isValidIndex(int type){
-    int res=0;
-    if(type==ImageGraphicsItem){
-        res=1;
-    }
-    if(type==TextBoxGraphicsItem){
-        res=1;
-    }
-    if(type==EdgeItem){
-        res=1;
-    }
-    if(type==BulletsItem){
-        res=1;
-    }
-    if(type==PicturesItems){
-        res=1;
-    }
-    return res;
+TextBoxItem* Save::setTextBoxItem(QSettings *settings,int i)
+{
+    QMap<QString, QVariant> data = m_formTextBoxes->getInfos();
+    return new TextBoxItem(data);
+    //m_scene.addItem(new TextBoxItem(data));
 }
 
+ArrowsGraphicsItem* Save::setArrowGraphicsItem(QSettings *settings,int i)
+{
+    /*bool WithoutAnchorPoint;
+    bool OneAnchorPoint;
+    bool TwoAnchorPoints;
+    int ArrowWidth;
+    int ArrowHeight;
+    int r,g,b,a;
+    WithoutAnchorPoint=settings->value(QString("item").append(QString::number(countItems)).append("/WithoutAnchorPoint")).toBool();
+    OneAnchorPoint=settings->value(QString("item").append(QString::number(countItems)).append("/OneAnchorPoint")).toBool();
+    TwoAnchorPoints=settings->value(QString("item").append(QString::number(countItems)).append("/TwoAnchorPoints")).toBool();
+    ArrowWidth=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowWidth")).toInt();
+    ArrowHeight=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowHeight")).toInt();
+    QColor ArrowOutlineColor(r,g,b,a);
+    r=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/r")).toInt();
+    g=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/g")).toInt();
+    b=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/b")).toInt();
+    a=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/a")).toInt();
+    r=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/r")).toInt();
+    g=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/g")).toInt();
+    b=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/b")).toInt();
+    a=settings->value(QString("item").append(QString::number(countItems)).append("/ArrowOutlineColor/a")).toInt();
+    QColor ArrowFillColor(r,g,b,a);
+    FormArrows formArrows();*/
+    //formArrows.fillColorArrowChanged(ArrowFillColo);
+    //formArrows.outlineColorArrowChanged(ArrowOutlineColor);
+    //ArrowsGraphicsItem *ArrowsGraphicsItem=new ArrowsGraphicsItem(formArrows());
+    return new ArrowsGraphicsItem(new FormArrows());
+}
+
+NumberedBulletGraphicItem* Save::setBulletsGraphicsItems(QSettings *settings,int i)
+{
+    int from (0), to (0), taille (0);
+    int shape (0);
+    QColor bulletcolor, numbercolor;
+    QFont qfont;
+    //m_formBullets.get_info(from, to, taille,  shape, bulletcolor, numbercolor, qfont);
+    NumberedBulletGraphicItem * numberedBulletGraphicItem (NULL);
+    qDebug () << "\tfrom == " << from << "\n";
+    qDebug () << "\tto == " << to << "\n";
+    int numbullet (from);
+    qreal posx (0), posy (50), delta (100);
+    for (; numbullet != to+1; ++numbullet) {
+      numberedBulletGraphicItem = new NumberedBulletGraphicItem (numbullet, (NumberedBulletGraphicItem::shape_e)shape, bulletcolor, numbercolor, qfont, taille);
+      numberedBulletGraphicItem->setPos(posx, posy);
+      return numberedBulletGraphicItem;
+    }
+}
+
+PicturesGraphicsItem* Save::setPicturesGraphicsItems(QSettings *settings,int i)
+{
+    return new PicturesGraphicsItem (m_formPicture);
+}
+
+GraphsGraphicsItem* Save::setCharts(QSettings *, int)
+{
+    GraphsInfo newGraphsInfo;
+    GraphsGraphicsItem *g = new GraphsGraphicsItem();
+    g->setInfos(newGraphsInfo);
+    return g;
+}
+
+///save function
 void Save::save()
 {
     QSettings settings(current_filename,QSettings::IniFormat);
+    settings.clear();
     countItems=0;
     foreach(QGraphicsItem *item,m_listItems){
         int type=item->type();
@@ -126,23 +216,20 @@ void Save::save()
             settings.setValue(QString("item").append(QString::number(countItems)).append("/rectF/height"),QString::number(height));
         }
 
-        if(type==ImageGraphicsItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-             getPicturesGraphicsItemData((PicturesGraphicsItem*)item);
-        }
-        if(type==TextBoxGraphicsItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-             getTextBoxItem((TextBoxItem*)item);
-        }
-
-        if(type==EdgeItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-             getArrowGraphicsItem((ArrowsGraphicsItem*)item);
-        }
-
-        if(type==BulletsItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-            getBulletsGraphicsItems((NumberedBulletGraphicItem *)item);
-        }
-
-        if(type==PicturesItems  && (x!=0 || y!=0 || width!=0 || height!=0)){
-            getPicturesGraphicsItems((PicturesGraphicsItem *)item);
+        switch(type)
+        {
+            case BaseGraphicItem::Type::TextBoxGraphicsItem:
+                getTextBoxItem((TextBoxItem*)item,&settings);
+            break;
+            case BaseGraphicItem::Type::ArrowGraphicsItem:
+                getArrowGraphicsItem((ArrowsGraphicsItem*)item,&settings);
+            break;
+            case BaseGraphicItem::NumberedBulletGraphicsItem:
+                getBulletsGraphicsItems((NumberedBulletGraphicItem *)item, &settings);
+            break;
+            case BaseGraphicItem::Type::ImageGraphicsItem:
+                getPicturesGraphicsItems((PicturesGraphicsItem *)item, &settings);
+            break;
         }
 
         if(type>65535 && (x!=0 || y!=0 || width!=0 || height!=0)){
@@ -152,6 +239,7 @@ void Save::save()
     settings.setValue("nbItems",countItems);
 }
 
+//open project function
 void Save::open()
 {
     int i;
@@ -170,23 +258,34 @@ void Save::open()
         double width=varWidth.toDouble();
         double height=varHeight.toDouble();
         QRectF rect(x,y,width,height);
-        if(type==ImageGraphicsItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-             //PicturesGraphicsItem *picture=new PicturesGraphicsItem();
-        }
-        if(type==TextBoxGraphicsItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-             //getTextBoxItem((TextBoxItem*)item);
-        }
-
-        if(type==EdgeItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-             //getArrowGraphicsItem((ArrowsGraphicsItem*)item);
-        }
-
-        if(type==BulletsItem  && (x!=0 || y!=0 || width!=0 || height!=0)){
-            //getBulletsGraphicsItems((NumberedBulletGraphicItem *)item);
-        }
-
-        if(type==PicturesItems  && (x!=0 || y!=0 || width!=0 || height!=0)){
-            //getPicturesGraphicsItems((PicturesGraphicsItem *)item);
+        ArrowsGraphicsItem *arrow;
+        TextBoxItem *texteBox;
+        NumberedBulletGraphicItem *bullet;
+        PicturesGraphicsItem *picturesItem;
+        GraphsGraphicsItem *graphsItem;
+        switch(type)
+        {
+            case BaseGraphicItem::Type::TextBoxGraphicsItem:
+                texteBox=setTextBoxItem(&settings,i);
+                m_scene->addItem(texteBox);
+            break;
+            case BaseGraphicItem::Type::ArrowGraphicsItem:
+                arrow=setArrowGraphicsItem(&settings,i);
+                m_scene->addItem(arrow);
+            break;
+            case BaseGraphicItem::NumberedBulletGraphicsItem:
+                bullet=setBulletsGraphicsItems(&settings,i);
+                m_scene->addItem(bullet);
+            break;
+            case BaseGraphicItem::Type::ImageGraphicsItem:
+                picturesItem=setPicturesGraphicsItems(&settings,i);
+                m_scene->addItem(picturesItem);
+            break;
+            case BaseGraphicItem::ChartGraphicsItem:
+                graphsItem=setCharts(&settings,i);
+                m_scene->addItem(graphsItem);
+            break;
         }
     }
 }
+
