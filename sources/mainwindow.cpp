56fd8f883a244e7f09b923cb8c4d04221b22013d
osap128 <olivier.sap@hotmail.fr>
2018-04-24 12:28:38 +0200

    rajout action set background color

--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -69,6 +69,7 @@ void MainWindow::buildMenu()
     connect(ui->actionOpen,             SIGNAL( triggered(bool) ),  this,   SLOT( openFile(bool) ));
     connect(ui->actionExportAs,         SIGNAL( triggered(bool) ),  this,   SLOT( exportView(bool) ));
     connect(ui->actionNew,              SIGNAL( triggered(bool) ),  this,   SLOT( slotNew(bool) ));
+    //connect(ui->action,              SIGNAL( triggered(bool) ),  this,   SLOT( slotNew(bool) ));
 
     connect(ui->actionArrow,            SIGNAL( triggered(bool) ),  this,   SLOT( actionClicked(bool) ));
     connect(ui->actionChart,            SIGNAL( triggered(bool) ),  this,   SLOT( actionClicked(bool) ));
