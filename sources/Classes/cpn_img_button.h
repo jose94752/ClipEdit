a6041fc65bdd0d89aafe4899767103610d2d226b
cmoliard <foetus333@gmail.com>
2018-04-26 10:22:48 +0200

    add component Img_Button

---
+++ sources/Classes/cpn_img_button.h
@@ -0,0 +1,67 @@
+#ifndef COMPONENT_IMG_BUTTON_H
+#define COMPONENT_IMG_BUTTON_H
+
+#include <QtWidgets/QWidget>
+
+#include <QImage>
+#include <QSize>
+
+
+// Forward declarations
+class QMouseEvent;
+class QPaintEvent;
+
+
+class My_Img_Button: public QWidget {
+    Q_OBJECT
+
+
+// Constructor
+public:
+
+    explicit My_Img_Button(QWidget* /*parent*/ = 0);
+
+
+public:
+
+    typedef enum e_IMG : unsigned char {
+        IMG_NONE = 0,
+        IMG_ERASER
+    } t_Img;
+
+
+// Interface
+public:
+
+    void set_image(t_Img);
+
+
+signals:
+
+    void on_click();
+
+
+// QWidget
+protected:
+
+    void mouseReleaseEvent(QMouseEvent*);
+
+    QSize minimumSizeHint() const { return QSize((int) img_width, (int) img_height); }
+
+    void paintEvent(QPaintEvent*);
+
+    QSize sizeHint() const { return QSize((int) img_width, (int) img_height); }
+
+
+// Data Members
+private:
+
+    QImage image;
+
+    t_Img  current_image;
+
+    size_t img_height, img_width;
+};
+
+
+#endif // COMPONENT_IMG_BUTTON_H
